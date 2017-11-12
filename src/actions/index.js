f552343cb25c33a58a223252fe12d9fb76273811
Mindaugas Lazauskas <mindaugaslazauskas@Mindaugass-MacBook-Pro.local>
2017-11-12 19:50:09 +0200

    initializing

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = 'c02d0b47befcade7031dec6d92a4f4f5';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	};
+}
